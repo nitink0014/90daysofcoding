69e1cba221e90d6c6e342ff9992331e625c73987
Nitin Kumar <nitinkumardbuu0014@gamil.com>
2026-07-30 09:17:55 +0530

    day 2

---
+++ src/StockProfit.java
@@ -0,0 +1,16 @@
+public class StockProfit {
+
+    public  static int maxProfit(int[] price){
+        int mminprice = price[0];
+        int
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
