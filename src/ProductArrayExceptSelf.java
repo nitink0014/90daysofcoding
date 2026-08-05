6344905ad1793dea8bd6c2e05d8e08ea6e0993bd
Nitin Kumar <nitinkumardbuu0014@gamil.com>
2026-08-05 10:31:35 +0530

    day 4

---
+++ src/ProductArrayExceptSelf.java
@@ -0,0 +1,15 @@
+public class ProductArrayExceptSelf {
+
+    public static int[] pro(int[] arr){
+        int maxProduct=0;
+
+
+        return new int[]{maxProduct};
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
